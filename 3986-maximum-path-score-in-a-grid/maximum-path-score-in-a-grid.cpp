5fb1096e02f891244b35b88258611faca0cd3bb3
Harsh Pashine <154049023+hdp01@users.noreply.github.com>
2026-05-01 00:03:27 +0530

    Time: 1243 ms (13.07%) | Memory: 499.8 MB (23.00%) - LeetSync

---
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxPathScore(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int inf = 1 << 30;
+        vector<vector<vector<int>>> f(m, vector<vector<int>>(n, vector<int>(k + 1, -1)));
+        function<int(int, int, int)> dfs = [&](int i, int j, int k) -> int {
+            if (i < 0 || j < 0 || k < 0) return -inf;
+            if (i == 0 && j == 0) return 0;
+            if (f[i][j][k] != -1) return f[i][j][k];
+            int res = grid[i][j];
+            int nk = k;
+            if (grid[i][j] > 0) --nk;
+            int a = dfs(i - 1, j, nk);
+            int b = dfs(i, j - 1, nk);
+            res += max(a, b);
+            return f[i][j][k] = res;
+        };
+        int ans = dfs(m - 1, n - 1, k);
+        return ans < 0 ? -1 : ans;
+    }
+};   
